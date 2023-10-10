f5e666c09cb055c1ddde2ede425924eaef6cd714
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-10-10 20:44:26 -0300

    Configura pagina de login

--- app/frontend/src/pages/index.tsx
+++ app/frontend/src/pages/index.tsx
@@ -1,12 +1,90 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import React, { useState } from 'react';
+import { Github, ShoppingCartIcon, Computer } from 'lucide-react'
+import { Separator } from "../components/ui/separator"
+import { Button } from "../components/ui/button"
 
 export default function Home() {
+  const [user, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e: any) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: any) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    // Aqui você pode adicionar a lógica para enviar os dados do login para o servidor
+    console.log('Email:', user);
+    console.log('Senha:', password);
+  };
+
+
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-    )
+    <div className="min-h-screen flex flex-col">
+      <header className="px-6 py-3 flex items-center justify-between border-b-2">
+
+        <h1 className="text-xl font-bold flex items-center">
+          <Computer 
+            className="w-4 h-4 mr-2"
+          />
+          PHCA.dev
+        </h1>
+
+        <h1 className="text-xl font-bold">SuperMarket App</h1>
+
+        <div className="flex items-center gap-3">
+          <Separator orientation="vertical" className="h-6" />
+          <Button variant="secondary">
+            <Github className="w-4 h-4 mr-2" />
+            <a href='https://github.com/dropeko' target='_blank'>GitHub</a>
+          </Button>
+        </div>
+      </header>
+
+
+      <main className="flex-1 p-6 flex gap-6 justify-center mt-40">
+        <div className='flex flex-col gap-4 border-white rounded-xl h-56 border justify-center p-4'>
+          <h2 className='flex justify-center text-xl'>Login:</h2>
+          <form onSubmit={handleSubmit} className=''>
+              <label className=''>
+                Usuário:
+                <input
+                  type="text"
+                  value={user}
+                  onChange={handleEmailChange}
+                  required
+                  className='mb-1 ml-1 text-black'
+                />
+              </label>
+              <br />
+              <label>
+                Senha:
+                <input
+                  type="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  required
+                  className='ml-1 text-black'
+                />
+              </label>
+              <br />
+              <div className='flex flex-auto items-center justify-center rounded-xl border border-white mt-2'>
+                <button type="submit" className=''>Entrar</button>
+              </div>
+            </form>
+          </div>
+        </main>
+
+      <footer className='flex justify-center'>
+        <span>
+          Desenvolvido com 💛 por PHCA.dev 
+        </span>
+      </footer>
+    </div>
+  )
 }
